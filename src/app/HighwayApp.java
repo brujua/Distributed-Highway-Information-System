6c026b48fc2036d9e1f60b80707fa22a37bc4f46
brujua <brunocrisafulli@yahoo.com.ar>
2018-12-11 23:02:00 -0300

    create new window for each HWNode

--- src/app/HighwayApp.java
+++ src/app/HighwayApp.java
@@ -1,30 +1,42 @@
 package app;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HighwayApp extends Application {
 
 	private static final Logger LOG = LoggerFactory.getLogger(HighwayApp.class);
+	private static final String TITLE = "HighWay App";
 
+	private Stage window;
+	private Scene mainScene;
+	private List<Stage> nodesWindows = new ArrayList<>();
 
 	@Override
 	public void start(Stage primaryStage) {
-		Button btn = new Button();
+		window = primaryStage;
+		window.setTitle(TITLE);
+
+		/*Button btn = new Button();
 		btn.setText("Log stuff");
 		btn.setOnAction(a-> {
 			LOG.info("This is some info");
 			LOG.error("This is some error");
 		});
+		TextArea textArea = new TextArea();
+		OutputStream os = new TextAreaOutputStream(textArea);
+		MyStaticOutputStreamAppender.setStaticOutputStream(os);
+		*/
 
 		Button btnNewCar = new Button();
 		btnNewCar.setText("Crear Auto");
@@ -38,41 +50,30 @@ public class HighwayApp extends Application {
 			createHWNode();
 		});
 
-		TextArea textArea = new TextArea();
-		OutputStream os = new TextAreaOutputStream(textArea);
 
-		MyStaticOutputStreamAppender.setStaticOutputStream(os);
 
 		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10));
+		grid.setVgap(10);
 		grid.add(btnNewCar,0,0);
 		grid.add(btnNewHW,1,0);
-		grid.add(textArea, 2 ,0);
-		grid.add(btn, 2, 1);
 
-		Scene mainScene = new Scene(grid, 800, 300);
+		mainScene = new Scene(grid, 800, 300);
 		primaryStage.setScene(mainScene);
 		primaryStage.show();
 	}
 
 	private void createHWNode() {
+
+		HWNodeWindow newWindow = new HWNodeWindow();
+
+
 	}
 
 	private void createCar() {
 	}
 
-	private static class TextAreaOutputStream extends OutputStream {
 
-		private TextArea textArea;
-
-		public TextAreaOutputStream(TextArea textArea) {
-			this.textArea = textArea;
-		}
-
-		@Override
-		public void write(int b) throws IOException {
-			textArea.appendText(String.valueOf((char) b));
-		}
-	}
 
 	public static void main(String[] args) {
 		launch(args);
