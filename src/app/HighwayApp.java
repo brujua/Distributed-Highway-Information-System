866dbce00c6830ca094891daaded9be4f630bf17
brujua <brunocrisafulli@yahoo.com.ar>
2019-01-25 18:42:51 -0300

    Add icon to view and applied dark theme

--- src/app/HighwayApp.java
+++ src/app/HighwayApp.java
@@ -7,11 +7,14 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class HighwayApp extends Application {
 
@@ -20,6 +23,8 @@ public class HighwayApp extends Application {
     private static final String CAR_TITLE = "Car";
     private static final String HWNODE_TITLE = "HW Node";
     private static final String COORD_TITLE = "Coordinator";
+    private static final String STYLE_FILE = "/css/dark.css";
+    private static final String ICON_FILE = "resources/hwnode2.png";
     private Scene mainScene;
     private Stage window;
     private BorderPane mainPane;
@@ -35,12 +40,14 @@ public class HighwayApp extends Application {
     public void start(Stage primaryStage) {
         window = primaryStage;
         window.setTitle(TITLE);
+        window.getIcons().add(new Image(new File(ICON_FILE).toURI().toString()));
         navigation = initNavigation();
         defaultCenter = initDefaultCenter();
 
         mainPane = new BorderPane();
         mainPane.setCenter(defaultCenter);
         mainScene = new Scene(mainPane);
+        mainScene.getStylesheets().add(STYLE_FILE);
 
         window.setScene(mainScene);
         window.setOnCloseRequest((event) -> {
