5bc2d6b65ff43b1151af55aac25a54984405118c
brujua <brunocrisafulli@yahoo.com.ar>
2019-02-04 20:40:40 -0300

    fix bug gui lagging if to much text its printed

--- src/app/LoggerTextArea.java
+++ src/app/LoggerTextArea.java
@@ -13,26 +13,43 @@ public class LoggerTextArea extends TextArea {
         super();
         this.setEditable(false);
         this.setPrefWidth(PREF_WIDHT);
-        MyStaticOutputStreamAppender.setStaticOutputStream(new TextAreaOutputStream(this));
+        TextAreaOutputStream outputStream = new TextAreaOutputStream(this);
+        MyStaticOutputStreamAppender.setStaticOutputStream(outputStream);
     }
 
     // Use to send logger text into the ui textArea
     public static class TextAreaOutputStream extends OutputStream {
 
+        private static final int MAX_LENGTH = 5000;
+        private static final int DELETE_SIZE = 1000;
         private TextArea textArea;
+        private StringBuffer buffer;
 
         public TextAreaOutputStream(TextArea textArea) {
             this.textArea = textArea;
+            buffer = new StringBuffer();
         }
-
 
         @Override
         public void write(int b) {
+            buffer.append((char) b);
+            String accumulated = buffer.toString();
+            if (accumulated.contains(System.lineSeparator())) {
+                sendToUI(accumulated);
+                buffer.delete(0, buffer.length()); //clean buffer
+            }
+        }
+
+        private void sendToUI(String str) {
             Platform.runLater(() -> {
                 int caretPosition = textArea.caretPositionProperty().get();
-                textArea.appendText(String.valueOf((char) b));
+                textArea.appendText(str);
                 textArea.positionCaret(caretPosition);
+                if (textArea.getLength() > MAX_LENGTH) {
+                    textArea.deleteText(0, DELETE_SIZE);
+                }
             });
+
         }
     }
 }
