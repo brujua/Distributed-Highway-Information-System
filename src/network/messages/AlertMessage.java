2552e4feee78bf4a22ec8dc1e62832d76069a18f
agrup <agustinrodriguez206@gmail.com>
2019-02-08 18:20:36 -0300

    ElertMessage AlertType created

---
+++ src/network/messages/AlertMessage.java
@@ -0,0 +1,45 @@
+package network.messages;
+
+import common.StNode;
+import network.Message;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class AlertMessage extends Message {
+
+    //private static final int  = ;
+    private String alertId;
+    private String msg;
+
+
+    public AlertMessage(StNode sender, AlertType alertType,String msg ) {
+        super(MessageType.ALERT, sender);
+        this.msg = msg;
+        this.alertId = UUID.randomUUID().toString();
+
+    }
+
+
+    public String getAlertId() {
+        return alertId;
+    }
+
+
+    public String getMsg() {
+        return msg;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AlertMessage that = (AlertMessage) o;
+        return alertId.equals(that.alertId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(alertId);
+    }
+}
---
+++ src/network/messages/AlertType.java
@@ -0,0 +1,5 @@
+package network.messages;
+
+public enum AlertType {
+    MaxSpeed
+}
