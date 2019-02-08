e14822f3f344759d2c7390ab7404c53ff634edf7
brujua <brunocrisafulli@yahoo.com.ar>
2019-02-08 04:58:32 -0300

    improve CarView visualisation of neighbour cars.
    Took 1 hour 20 minutes

--- src/app/CarDrawer.java
+++ src/app/CarDrawer.java
@@ -7,6 +7,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,6 +23,12 @@ public class CarDrawer implements Drawer {
     private static final String NEIGH_IMG_FILE = "resources/car3.png";
     private static final String CAR_IMG_FILE = "resources/car.png";
     private static final long MIN_TIME_BEFORE_UPDATE = 1000;
+    private static final double SCALE_FACTOR_X = 4;
+    private static final double SCALE_FACTOR_Y = 1;
+    private static final double TRIANGLE_WIDTH = 10;
+    private static final double TRIANGLE_HEIGHT = 7;
+    private static final double NEIGH_FONT_SIZE = 11;
+    private static final double ETIQUETTE_MAX_WIDHT = 100;
     private Image neighImg;
     private Image carImg;
 
@@ -38,6 +45,18 @@ public class CarDrawer implements Drawer {
 
     }
 
+    private void initImages() {
+        try {
+            //the image has a 2:1 ratio
+            neighImg = new Image(new File(NEIGH_IMG_FILE).toURI().toString(), 40, 20, true, true);
+            carImg = new Image(new File(CAR_IMG_FILE).toURI().toString(), 50, 25, true, true);
+            logger.debug("Car Imgs initialized and scaled");
+        } catch (IllegalArgumentException e) {
+            logger.error("Could not initialize image: " + e.getMessage());
+        }
+    }
+
+    @Override
     public void draw(Canvas canvas, long currentTime) {
         if (updateNeeded(currentTime)) {
             drawBackground(canvas);
@@ -48,15 +67,18 @@ public class CarDrawer implements Drawer {
             //draw car in the middle
             gc.drawImage(carImg, middleCanvasPosition.getCordx() - (carImg.getWidth() / 2), middleCanvasPosition.getCordy() - (carImg.getHeight() / 2));
             for (CarStNode neigh : neighs) {
-                Position neighPos = neigh.getPosition();
-                Position relativePos = new Position(
-                        neighPos.getCordx() - carPosition.getCordx() + middleCanvasPosition.getCordx(),
-                        neighPos.getCordy() - carPosition.getCordy() + middleCanvasPosition.getCordy());
-                gc.drawImage(neighImg, relativePos.getCordx() - (neighImg.getWidth() / 2), relativePos.getCordy() - (neighImg.getHeight() / 2));
+                drawNeighbourCar(carPosition, neigh, middleCanvasPosition, canvas);
             }
         }
+    }
 
-
+    private boolean updateNeeded(long currentTime) {
+        if (previousTime == 0) {
+            previousTime = currentTime;
+            return true;
+        }
+        long timeEnlapsed = currentTime - previousTime;
+        return timeEnlapsed > MIN_TIME_BEFORE_UPDATE;
     }
 
     private void drawBackground(Canvas canvas) {
@@ -69,23 +91,35 @@ public class CarDrawer implements Drawer {
         return new Position(canvas.getWidth() / 2, canvas.getHeight() / 2);
     }
 
-    private boolean updateNeeded(long currentTime) {
-        if (previousTime == 0) {
-            previousTime = currentTime;
-            return true;
+    private void drawNeighbourCar(Position carPos, CarStNode neigh, Position canvasMiddlePosition, Canvas canvas) {
+        Position neighPos = neigh.getPosition();
+        double scaledCoordX = ((neighPos.getCordx() - carPos.getCordx()) * SCALE_FACTOR_X) + canvasMiddlePosition.getCordx();
+        double scaledCoordY = ((neighPos.getCordy() - carPos.getCordy()) * SCALE_FACTOR_Y) + canvasMiddlePosition.getCordy();
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        //Warning! Assuming that scaledCoordY never exceeds canvas bounds. if it doest, wont be drawn
+
+        gc.setFill(Color.GREEN);
+        gc.setFont(new Font(NEIGH_FONT_SIZE));
+        String etiquette = getNeighEtiquette(neigh);
+        double etiquetteYAdjusted = scaledCoordY + (NEIGH_FONT_SIZE / 3.0); //arbitrary adjustment
+        if (scaledCoordX > canvas.getWidth()) {// out of canvas by the right
+            gc.fillPolygon(new double[]{canvas.getWidth() - TRIANGLE_WIDTH, canvas.getWidth() - 1, canvas.getWidth() - TRIANGLE_WIDTH},
+                    new double[]{scaledCoordY + TRIANGLE_HEIGHT, scaledCoordY, scaledCoordY - TRIANGLE_HEIGHT},
+                    3);
+            gc.fillText(etiquette, canvas.getWidth() - TRIANGLE_WIDTH - ETIQUETTE_MAX_WIDHT, etiquetteYAdjusted, ETIQUETTE_MAX_WIDHT);
+        } else if (scaledCoordX < 0) { //out of canvas by the left
+            gc.fillPolygon(new double[]{TRIANGLE_WIDTH, 1, TRIANGLE_WIDTH},
+                    new double[]{scaledCoordY + TRIANGLE_HEIGHT, scaledCoordY, scaledCoordY - TRIANGLE_HEIGHT}, 3);
+            gc.fillText(etiquette, TRIANGLE_WIDTH + 2, etiquetteYAdjusted, ETIQUETTE_MAX_WIDHT); //arbitrary adjustment
+        } else { //within bounds of the canvas
+            gc.drawImage(neighImg, scaledCoordX - (neighImg.getWidth() / 2), scaledCoordY - (neighImg.getHeight() / 2));
+            gc.fillText(etiquette, scaledCoordX - (neighImg.getWidth()), scaledCoordY - (neighImg.getHeight() / 2));
         }
-        long timeEnlapsed = currentTime - previousTime;
-        return timeEnlapsed > MIN_TIME_BEFORE_UPDATE;
+
     }
 
-    private void initImages() {
-        try {
-            //the image has a 2:1 ratio
-            neighImg = new Image(new File(NEIGH_IMG_FILE).toURI().toString(), 60, 30, true, true);
-            carImg = new Image(new File(CAR_IMG_FILE).toURI().toString(), 60, 30, true, true);
-            logger.debug("Car Imgs initialized and scaled");
-        } catch (IllegalArgumentException e) {
-            logger.error("Could not initialize image: " + e.getMessage());
-        }
+    private String getNeighEtiquette(CarStNode neigh) {
+        return "#" + neigh.getId().substring(0, 5) + " vel:" + neigh.getPulse().getVelocity() + "Km/h";
     }
 }
